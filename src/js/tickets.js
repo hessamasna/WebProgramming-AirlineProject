5102641d33c224ed6bad03306fe70fb79edf03d7
Hesam Asnaashari <hessamasna@yahoo.com>
2022-12-08 17:03:02 +0330

    [FIX] fix Tickets js & html

--- src/js/tickets.js
+++ src/js/tickets.js
@@ -1,3 +1,12 @@
+const TICKET_SEARCH_BTN = document.getElementById("ticket-search");
+const FLIGHT_MODE = document.getElementById("flightMode");
+const ORIGIN = document.getElementById("origin");
+const DESTINATION = document.getElementById("destination");
+const START_DATE = document.getElementById("startDate");
+const FINISH_DATE = document.getElementById("finishDate");
+const PASSENGER_NUMBER = document.getElementById("PassengerNumber");
+const FLIGHT_TYPE = document.getElementById("flightType");
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const params = Object.fromEntries(urlParams)
@@ -11,13 +20,16 @@ const availableTickets = [
         duration: 180,// TODO use to calculate arrival time
         company: 'Qatar Airways',
         takeOff: '20:00',
+        startDate: '22/11/2022',
         arrival: '21:00',
+        finishDate: '22/11/2022',
         pricePerPerson: 200000,
         numberOfPerson: 4,
         capacity: 100,
         fullPrice: this.numberOfPerson * this.pricePerPerson,
         isCapacityLimited: this.capacity < (this.numberOfPerson * 3),
-        flightType: 'Business'
+        flightType: 'Business',
+        flightMode: 'twoWay'
     }
 ]
 availableTickets.map(ticket => {
@@ -88,3 +100,69 @@ availableTickets.map(ticket => {
                     </div>
     `
 })
+
+//******************************************//
+
+FLIGHT_MODE.value = params.flightMode;
+ORIGIN.value = params.origin;
+DESTINATION.value = params.destination;
+PASSENGER_NUMBER.value = params.PassengerNumber;
+START_DATE.value = params.startDate;
+FINISH_DATE.value = params.finishDate;
+FLIGHT_TYPE.value = params.flightType;
+
+TICKET_SEARCH_BTN.addEventListener("click", function() {
+    let params = '?'
+    // if (!!ORIGIN || !!DESTINATION || !!START_DATE || !!FINISH_DATE){
+    //     return;
+    // }
+
+    params += 'flightMode='+FLIGHT_MODE.value+'&';
+    params += 'origin='+ORIGIN.value+'&';
+    params += 'destination='+DESTINATION.value+'&';
+    params += 'PassengerNumber='+PASSENGER_NUMBER.value+'&';
+    params += 'startDate='+START_DATE.value+'&';
+    params += 'finishDate='+FINISH_DATE.value+'&';
+    params += 'flightType='+FLIGHT_TYPE.value+'&';
+
+    console.log(params)
+    window.location.href ='/tickets.html'+params;
+});
+
+function decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+        'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value--;
+    if (value<=0)
+        value =1;
+    target.value = value;
+}
+
+function increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+        'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+}
+
+const decrementButtons = document.querySelectorAll(
+    `button[data-action="decrement"]`
+);
+
+const incrementButtons = document.querySelectorAll(
+    `button[data-action="increment"]`
+);
+
+decrementButtons.forEach(btn => {
+    btn.addEventListener("click", decrement);
+});
+
+incrementButtons.forEach(btn => {
+    btn.addEventListener("click", increment);
+});
