6dfc2873675e60f8ef65f542e18b56a0d921f78e
Shayan Shabanzadeh <shayanshabanzadeh@Shayans-MacBook-Pro-2.local>
2022-12-08 22:04:59 +0330

    add login and register page

---
+++ src/js/loginAndRegister.js
@@ -0,0 +1,62 @@
+function setFormMessage(formElement, _type , message){
+    const messageElement = formElement.querySelector('.form__message');
+
+    messageElement.textContent = message;
+    messageElement.classList.remove('form__message--success' , 'form__messsage--error');
+    messageElement.classList.add('form__message--${_type}');
+}
+
+
+function setInputError(inputElement, message){
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement){
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+
+}
+
+
+document.addEventListener("DOMContentLoaded",() => {
+    const loginForm = document.querySelector("#login")
+    const createAccountForm = document.querySelector("#createAccount")
+
+    document.querySelector("#LinkCreateAccount").addEventListener("click",e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+
+    document.querySelector("#LinkLogin").addEventListener("click",e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+    createAccountForm.addEventListener("submit" , e => {
+        e.preventDefault();
+        const fd = new FormData(createAccountForm);
+        const obj = Object.fromEntries(fd);
+        
+        const json = JSON.stringify(obj);
+        localStorage.setItem('signupForm' , json);
+    })
+
+    loginForm.addEventListener("submit",e => {
+        e.preventDefault();
+        
+        setFormMessage(loginForm , "success" , "شما با موفقیت وارد شدید");
+    });
+
+    // document.querySelectorAll(".form__input").forEach(inputElement => {
+    //     inputElement.addEventListener("blur",e => {
+    //         if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5){
+    //             setInputError(inputElement , "Username must be at least 5 characters");
+    //         }
+    //     });
+    //     inputElement.addEventListener("input" , e => {clearInputError(inputElement)} );
+    // });
+
+})
