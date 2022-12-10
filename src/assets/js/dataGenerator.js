ab08a2575bb4e74854c42da674d89a4539bc4043
Amirzgh1 <Amirgh5665@gmail.com>
2022-12-10 21:34:31 +0330

    update data generator

--- src/assets/js/dataGenerator.js
+++ src/assets/js/dataGenerator.js
@@ -18,14 +18,14 @@ const durations = [
 const flightClass = [
     'economy','business','firstClass'
 ]
-const data = [];
+const MOCK_DATA = [];
 
 
 for (let i = 0; i < 300; i++) {
     let tempAirports = airports;
     const startDate = randomDate();
-    const end = randomDate(start = startDate)
-    data.push({
+    const end = randomDate(startDate)
+    MOCK_DATA.push({
         id: i + 1,
         flightNUmber: Math.floor(1000 + Math.random() * 9000),
         origin: tempAirports[Math.floor((Math.random() * tempAirports.length))],
@@ -36,15 +36,15 @@ for (let i = 0; i < 300; i++) {
         arrival: Math.floor(Math.random() * 24),
         startDate: startDate.toISOString().split('T')[0],
         finishDate: end.toISOString().split('T')[0],
-        numberOfPerson: Math.floor(Math.random()*6),
+        pricePerPerson: Math.floor(Math.random() * (1_000_000 - 600_000 + 1) + 600_000),
         capacity: Math.floor(Math.random() * (110 - 20 + 1)) + 20,
         flightType: flightClass[Math.floor((Math.random()*flightClass.length))]
     })
 }
 
-console.log(data);
 // navigator.clipboard.write(data)
 
-function randomDate(start=new Date(2022,12,10),end=new Date(2023,12,0)) {
+function randomDate(start=new Date(2022,12,10)) {
+    const end= new Date(2023,12,0);
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
